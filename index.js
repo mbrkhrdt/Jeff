27dcf7bb50da799fe5ba209fefcf34f1cc5c36dd
Michael Burkhardt <michael@peroxaan.com>
2024-05-11 18:33:08 -0400

    Basic Discord

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 require('dotenv').config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { Client, GatewayIntentBits } = require("discord.js")
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
+
+let prompt = "You are a chatbot in a Discord server, and your name is Jeff. You'll be talking to random people in a singular channel. You're able to chat about whatever people ask about, but keep it PG and don't use any harmful language. Have fun and keep it entertaining! The following is whats been said so far, but only respond to the last thing thats been said:"
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+    if (message.channel.name == "jeff") {
+        message.reply('Hello!');
+        return;
+    }
+});
+
+client.login(process.env.DiscordAPIKey);
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GoogleAPIKey);
@@ -11,12 +37,9 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro"});
 
 // ...
 async function run() {
-    const prompt = "You are a chatbot in a Discord server, and your name is Jeff. You'll be talking to random people in a singular channel. You're able to chat about whatever people ask about, but keep it PG and don't use any harmful language. Have fun and keep it entertaining! The following is whats been said so far, but only respond to the last thing thats been said:"
-  
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
     console.log(text);
   }
   
-  run();
